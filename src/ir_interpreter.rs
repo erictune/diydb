1944f649db2039d529d4d3fe31a25623528a5ac0
Eric Tune <eric.tune@gmail.com>
2023-06-16 07:52:36 -0700

    Use generic fn in ir_interpreter
    Reduces duplicated source code, while
    keeping inner loop free of matches
    (compared to having an enum-dispatched iterator).

--- src/ir_interpreter.rs
+++ src/ir_interpreter.rs
@@ -1,7 +1,9 @@
 //! executes SQL intermediate representation (IR).
 
 use anyhow::{Context, Result};
+use streaming_iterator::StreamingIterator;
 
+use crate::ast;
 use crate::ir;
 use crate::pager;
 use crate::project;
@@ -12,73 +14,58 @@ use crate::table::Table;
 use crate::typed_row::Row;
 use crate::TempTable;
 
+fn project_any_table_into_temp_table<T, I>(in_tbl: &T, in_it: I, out_cols: &[ast::SelItem]) -> Result<crate::TempTable>
+where
+    T: TableMeta,
+    I: StreamingIterator<Item = Row>,
+{
+    let (actions, column_names, column_types) =
+    project::build_project(&in_tbl.column_names(), &in_tbl.column_types(), out_cols)?;
+    let mut it = in_it.map(|row| project::project_row(&actions, row));
+    let mut rows: Vec<Row> = vec![];
+    loop {
+        it.advance();
+        if it.get().is_none() {
+            break;
+        }
+        let res = it.get().unwrap().as_ref();
+        match res {
+            Err(e) => {
+                return Err(anyhow::anyhow!(format!("Not able to convert value: {}", e)))
+            }
+            Ok(r) => rows.push(r.clone()),
+        }
+    }
+    Ok(TempTable {
+        rows,
+        table_name: String::from("?unnamed?"),
+        column_names,
+        column_types,
+        strict: false,  // SQLite defaults to non-strict, so result tables (without an explicit CREATE) shall be non-strict.
+    })
+}
+
 /// Run an IR representation of a query, returning a TempTable with the results of the query.
 pub fn run_ir(ps: &pager::PagerSet, ir: &ir::Block) -> Result<crate::TempTable> {
-    use streaming_iterator::StreamingIterator;
-
     match ir {
         ir::Block::Project(p) => {
             let child = p
                 .input
                 .as_scan()
                 .context("Project should only have Scan as child")?;
-            let (rows, column_names, column_types) = match child.databasename == "temp" {
+            match child.databasename == "temp" {
                 true => {
                     let tbl = ps.get_temp_table(&child.tablename)?;
-                    // TODO: reduce redundancy between this arm and the next  by making a generic function like this:
-                    // `f<T>() where T: TableMeta + RowStream`, which does the steps in the remainder of this arm.
-                    let (actions, column_names, column_types) =
-                    project::build_project(&tbl.column_names(), &tbl.column_types(), &p.outcols)?;
-                    let mut it = tbl
-                        .streaming_iterator()
-                        .map(|row| project::project_row(&actions, row));
-                    let mut rows: Vec<Row> = vec![];
-                    loop {
-                        it.advance();
-                        if it.get().is_none() {
-                            break;
-                        }
-                        let res = it.get().unwrap().as_ref();
-                        match res {
-                            Err(e) => {
-                                return Err(anyhow::anyhow!(format!("Not able to convert value: {}", e)))
-                            }
-                            Ok(r) => rows.push(r.clone()),
-                        }
-                    }
-                    (rows, column_names, column_types)
+                    let base_it = tbl.streaming_iterator();
+                    project_any_table_into_temp_table(tbl, base_it, &p.outcols)
                 }
                 false => {
+                    // TODO: this should be a reference to a Table held by the DB, not a Table created here on the stack.
                     let tbl: Table<'_> = Table::open_read(ps.default_pager()?, child.tablename.as_str())?;
-                    let (actions, column_names, column_types) =
-                    project::build_project(&tbl.column_names(), &tbl.column_types(), &p.outcols)?;
-                    let mut it = tbl
-                        .streaming_iterator()
-                        .map(|row| project::project_row(&actions, row));
-                    let mut rows: Vec<Row> = vec![];
-                    loop {
-                        it.advance();
-                        if it.get().is_none() {
-                            break;
-                        }
-                        let res = it.get().unwrap().as_ref();
-                        match res {
-                            Err(e) => {
-                                return Err(anyhow::anyhow!(format!("Not able to convert value: {}", e)))
-                            }
-                            Ok(r) => rows.push(r.clone()),
-                        }
-                    }
-                    (rows, column_names, column_types)
+                    let base_it = tbl.streaming_iterator();
+                    project_any_table_into_temp_table(&tbl, base_it, &p.outcols)
                 }
-            };
-            Ok(TempTable {
-                rows,
-                table_name: String::from("?unnamed?"),
-                column_names,
-                column_types,
-                strict: false,  // SQLite defaults to non-strict, so result tables (without an explicit CREATE) shall be non-strict.
-            })
+            }
         }
         ir::Block::ConstantRow(cr) => {
             return Ok(TempTable {
